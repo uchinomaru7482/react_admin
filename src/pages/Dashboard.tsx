0e3a01b48f62f9fb8466c6443061b1774ddced79
Uchinomaru <bravecred@gmail.com>
2022-03-26 17:29:52 +0900

    不要コード削除

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,36 +1,10 @@
-import React, { useRef, useContext } from 'react'
-import Modal from '../components/Modal'
-import { ToastContext, ToastKind } from '../contexts/ToastContext'
-
-interface Handler {
-  showModal(): void
-}
+import React from 'react'
 
 const Dashboard: React.VFC = () => {
-  const childRef = useRef({} as Handler)
-  const toast = useContext(ToastContext)
-
   return (
     <>
       <div className='frame'>
         <h2 className='page-title'>Dashboard</h2>
-        <Modal
-          title='Title'
-          body='Body'
-          callback={() => console.log('callback')}
-          ref={childRef}
-        />
-        <button onClick={() => childRef.current.showModal()}>
-          Display Modal
-        </button>
-        <div>
-          <button onClick={() => toast.addToast(ToastKind.Success)}>
-            Success
-          </button>
-        </div>
-        <div>
-          <button onClick={() => toast.addToast(ToastKind.Error)}>Error</button>
-        </div>
       </div>
     </>
   )
