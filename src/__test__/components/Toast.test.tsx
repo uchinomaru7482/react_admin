eb44d6c7ab7d870b6d23e565d9ad0c31f7ba9d48
Uchinomaru <bravecred@gmail.com>
2022-03-30 00:43:56 +0900

    Toastコンポーネントのテスト追加

---
+++ src/__test__/components/Toast.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react'
+import Toast from '../../components/Toast'
+
+describe('Rendering', () => {
+  test('Toast snap shot test', () => {
+    const { asFragment } = render(<Toast toastType='Success' indexNum={1} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('Toast type Success', () => {
+    const { container } = render(<Toast toastType='Success' indexNum={1} />)
+    expect(container.innerHTML).toMatch('Execution completed.')
+    expect(
+      container.getElementsByClassName('border-indigo-300').length
+    ).toEqual(2)
+  })
+
+  test('Toast type Error', () => {
+    const { container } = render(<Toast toastType='Error' indexNum={1} />)
+    expect(container.innerHTML).toMatch('Error has occurred.')
+    expect(container.getElementsByClassName('border-pink-300').length).toEqual(
+      2
+    )
+  })
+})
