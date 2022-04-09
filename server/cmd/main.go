3c77c161f6924d23512d3ec55891c7f909a99bc4
Uchinomaru <bravecred@gmail.com>
2022-04-09 15:38:29 +0900

    axios設定追加

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"net/http"
 	"react_admin/src/interfaces"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 	e.GET("/user", interfaces.GetUsers)
 	e.POST("/user", interfaces.CreateUsers)
 	e.DELETE("/user/:id", interfaces.DeleteUsers)
